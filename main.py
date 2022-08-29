a8673438fd41bdfb10754fcb9237ef3507db784d
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-08-29 13:53:42 -0500

    more uploaded files
    continued python learning!!!!

---
+++ main.py
@@ -0,0 +1,19 @@
+# this is how you write a comment
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+
+print("Hello " + full_name)
+
+age = 21
+age += 1
+print("Your age is " + str(age))
+
+
+height = 250.5
+print("Your height is " + str(height) + "cm")
+
+human = True
+print("Are you human? " + str(human))
+
