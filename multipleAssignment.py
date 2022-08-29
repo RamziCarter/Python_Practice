2544d7ad655e0f13035ceac91b8c0d5d1269334a
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-08-29 13:56:42 -0500

    Add files via upload

---
+++ multipleAssignment.py
@@ -0,0 +1,22 @@
+# multiple assignement
+# allows you to assign multiple variables at the same time
+# with only one line of code
+
+
+#name = "Ramzi"
+#age = 23
+#birthday_Month = "October"
+
+
+name, age, birthday_Month = "Ramzi", 23, "October"
+
+print("Hello " + name + " you are " + str(age) + " and your birthday is in " + birthday_Month)
+
+#assigning multiple variables the same value
+
+Dad = Mom = Sister = Cousin = Me = 5
+print(Dad)
+print(Mom)
+print(Me)
+print(Sister)
+print(Cousin)
---
+++ stringMethods.py
@@ -0,0 +1,12 @@
+name = "ramziCarterJr"
+#print(len(name))
+
+#print(name.find("a"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("a"))
+#print(name.replace("a" , "z")
+print(name * 5)
