33883ed6cdbfe0de2e38aa7584f1b177493506c1
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-12-03 23:13:33 -0800

    Update variables.py
    Learning how to use variables in python and the different types

--- variables.py
+++ variables.py
@@ -1,4 +1,6 @@
 # this is how you write a comment
+# Tip: Be consistent!
+
 
 first_name = "Bro"
 last_name = "Code"
@@ -17,3 +19,18 @@ print("Your height is " + str(height) + "cm")
 human = True
 print("Are you human? " + str(human))
 
+print("Today is Saturday, I went to chick-fil-a and purchased a spicy chicken sandwich and milkshake! ")
+
+can = "sprite!"
+person = "Man"
+Action = " has a drink, which is a "
+
+full_Action = person + Action + can
+print(full_Action)
+
+
+FavoriteNumber = 22
+
+print("Your favorite number is " + str(FavoriteNumber))
+
+print(type(FavoriteNumber))
