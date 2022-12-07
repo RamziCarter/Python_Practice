66aee85f82937756ca9fc2c7903fb0a70c066a96
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-12-06 22:45:36 -0800

    Update typeCasting.py

--- typeCasting.py
+++ typeCasting.py
@@ -1,13 +1,20 @@
 #type casting is the convertion of a one data type to another
 
 x = 100 #integer
+
 y = 4.00 # float
+
 z = "5" #string
+
 x = float(x)
+
 y = float(y)
+
 z = float(z)
 
 
 print("The value for x is " + str(x))
+
 print(y)
-print(z*5)
+
+print(z * 5)
