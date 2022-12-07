f21d50452fb9340aa3dfe4fdcc21b71e54c47ea3
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-12-06 22:38:30 -0800

    Update stringMethods.py

--- stringMethods.py
+++ stringMethods.py
@@ -1,12 +1,21 @@
 name = "ramziCarterJr"
-#print(len(name))
 
-#print(name.find("a"))
-#print(name.capitalize())
-#print(name.upper())
-#print(name.lower())
-#print(name.isdigit())
-#print(name.isalpha())
-#print(name.count("a"))
-#print(name.replace("a" , "z")
+print(len(name))
+
+print(name.find("a")) # first index where character is
+
+print(name.capitalize()) # only first letter is upper case
+
+print(name.upper()) #uppercase entire string
+
+print(name.lower()) # lowercase entire string
+
+print(name.isdigit()) # true or false
+
+print(name.isalpha()) # returns false if string contains anything other than alphabetical letters
+
+print(name.count("a")) # counts occurrences of a letter or symbol
+
+print(name.replace("a" , "z"))
+
 print(name * 5)
